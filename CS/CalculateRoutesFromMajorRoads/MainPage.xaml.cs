62874deb6ff284af49a40285a0a0e83598bda746
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CalculateRoutesFromMajorRoads/MainPage.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Map;
+
+namespace CalculateRoutesFromMajorRoads {
+    public partial class MainPage : UserControl {
+        GeoPoint receiveLocationLocation = new GeoPoint(40, -120);
+        public GeoPoint ReceiveLocation {
+            get { return receiveLocationLocation; }
+            set { receiveLocationLocation = value; }
+        }
+
+        public MainPage() {
+            InitializeComponent();
+            this.DataContext = ReceiveLocation;
+        }
+
+        #region #CalculateRoutesFromMajorRoads
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            routeProvider.CalculateRoutesFromMajorRoads(
+                new RouteWaypoint("Destination Point", receiveLocationLocation));
+        }
+        #endregion #CalculateRoutesFromMajorRoads
+
+        #region #RouteCalculated
+        private void routeProvider_RouteCalculated(object sender, BingRouteCalculatedEventArgs e) {
+            RouteCalculationResult result = e.CalculationResult;
+
+            StringBuilder resultList = new StringBuilder();
+            resultList.Append(String.Format("Status: {0}\n", result.ResultCode));
+            resultList.Append(String.Format("Fault reason: {0}\n", result.FaultReason));
+            resultList.Append(ProcessStartingPoints(result.StartingPoints));
+            resultList.Append(ProcessRouteResults(result.RouteResults));
+
+            tbResult.Text = resultList.ToString();
+        }
+        #endregion #RouteCalculated
+
+        #region #ProcessStartingPoints
+        string ProcessStartingPoints(List<RouteWaypoint> points) {
+            if (points == null) return "";
+
+            StringBuilder sb = new StringBuilder("Starting Points:\n");
+            sb.Append(String.Format("_________________________\n"));
+            for (int i = 0; i < points.Count; ++i)
+                sb.Append(String.Format(
+                    "Starting point {0}: {1} ({2})\n",
+                    i + 1,
+                    points[i].Description,
+                    points[i].Location
+                ));
+            return sb.ToString();
+        }
+        #endregion #ProcessStartingPoints
+
+        #region #ProcessRouteResults
+        string ProcessRouteResults(List<BingRouteResult> results) {
+            if (results == null) return "";
+
+            StringBuilder sb = new StringBuilder("RouteResults:\n");
+            for (int i = 0; i < results.Count; i++) {
+                sb.Append(String.Format("_________________________\n"));
+                sb.Append(String.Format("Path {0}:\n", i + 1));
+                sb.Append(String.Format("Distance: {0}\n", results[i].Distance));
+                sb.Append(String.Format("Time: {0}\n", results[i].Time));
+                sb.Append(ProcessLegs(results[i].Legs));
+            }
+            return sb.ToString();
+        }
+        #endregion #ProcessRouteResults
+
+        #region #ProcessLegs
+        string ProcessLegs(List<BingRouteLeg> legs) {
+            if (legs == null) return "";
+
+            StringBuilder sb = new StringBuilder("Legs:\n");
+            for (int i = 0; i < legs.Count; i++) {
+                sb.Append(String.Format("\tLeg {0}:\n", i + 1));
+                sb.Append(String.Format("\tStart: {0}\n", legs[i].StartPoint));
+                sb.Append(String.Format("\tEnd: {0}\n", legs[i].EndPoint));
+                sb.Append(String.Format("\tDistance: {0}\n", legs[i].Distance));
+                sb.Append(String.Format("\tTime: {0}\n", legs[i].Time));
+                sb.Append(ProcessItinerary(legs[i].Itinerary));
+            }
+            return sb.ToString();
+        }
+        #endregion #ProcessLegs
+
+        #region #ProcessItinerary
+        string ProcessItinerary(List<BingItineraryItem> items) {
+            if (items == null) return "";
+
+            StringBuilder sb = new StringBuilder("\tInternary Items:\n");
+            for (int i = 0; i < items.Count; i++) {
+                sb.Append(String.Format("\t\tItinerary {0}:\n", i + 1));
+                sb.Append(String.Format("\t\tManeuver: {0}\n", items[i].Maneuver));
+                sb.Append(String.Format("\t\tLocation: {0}\n", items[i].Location));
+                sb.Append(String.Format("\t\tInstructions: {0}\n", items[i].ManeuverInstruction));
+                sb.Append(ProcessWarnings(items[i].Warnings));
+            }
+            return sb.ToString();
+        }
+        #endregion #ProcessItinerary
+
+        #region #ProcessWarnings
+        string ProcessWarnings(List<BingItineraryItemWarning> warnings) {
+            if (warnings == null) return "";
+
+            StringBuilder sb = new StringBuilder("\t\tWarnings:\n");
+            for (int i = 0; i < warnings.Count; i++) {
+                sb.Append(String.Format("\t\t\tWarning {0}:\n", i + 1));
+                sb.Append(String.Format("\t\t\tType: {0}\n", warnings[i].Type));
+                sb.Append(String.Format("\t\t\tText: {0}\n", warnings[i].Text));
+
+            }
+            return sb.ToString();
+        }
+        #endregion #ProcessWarnings
+    }
+}
---
+++ VB/CalculateRoutesFromMajorRoads/MainPage.xaml.vb
@@ -0,0 +1,134 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Map
+
+Namespace CalculateRoutesFromMajorRoads
+    Partial Public Class MainPage
+        Inherits UserControl
+
+        Private receiveLocationLocation As New GeoPoint(40, -120)
+        Public Property ReceiveLocation() As GeoPoint
+            Get
+                Return receiveLocationLocation
+            End Get
+            Set(ByVal value As GeoPoint)
+                receiveLocationLocation = value
+            End Set
+        End Property
+
+        Public Sub New()
+            InitializeComponent()
+            Me.DataContext = ReceiveLocation
+        End Sub
+
+        #Region "#CalculateRoutesFromMajorRoads"
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            routeProvider.CalculateRoutesFromMajorRoads(New RouteWaypoint("Destination Point", receiveLocationLocation))
+        End Sub
+        #End Region ' #CalculateRoutesFromMajorRoads
+
+        #Region "#RouteCalculated"
+        Private Sub routeProvider_RouteCalculated(ByVal sender As Object, ByVal e As BingRouteCalculatedEventArgs)
+            Dim result As RouteCalculationResult = e.CalculationResult
+
+            Dim resultList As New StringBuilder()
+            resultList.Append(String.Format("Status: {0}" & ControlChars.Lf, result.ResultCode))
+            resultList.Append(String.Format("Fault reason: {0}" & ControlChars.Lf, result.FaultReason))
+            resultList.Append(ProcessStartingPoints(result.StartingPoints))
+            resultList.Append(ProcessRouteResults(result.RouteResults))
+
+            tbResult.Text = resultList.ToString()
+        End Sub
+        #End Region ' #RouteCalculated
+
+        #Region "#ProcessStartingPoints"
+        Private Function ProcessStartingPoints(ByVal points As List(Of RouteWaypoint)) As String
+            If points Is Nothing Then
+                Return ""
+            End If
+
+            Dim sb As New StringBuilder("Starting Points:" & ControlChars.Lf)
+            sb.Append(String.Format("_________________________" & ControlChars.Lf))
+            For i As Integer = 0 To points.Count - 1
+                sb.Append(String.Format("Starting point {0}: {1} ({2})" & ControlChars.Lf, i + 1, points(i).Description, points(i).Location))
+            Next i
+            Return sb.ToString()
+        End Function
+        #End Region ' #ProcessStartingPoints
+
+        #Region "#ProcessRouteResults"
+        Private Function ProcessRouteResults(ByVal results As List(Of BingRouteResult)) As String
+            If results Is Nothing Then
+                Return ""
+            End If
+
+            Dim sb As New StringBuilder("RouteResults:" & ControlChars.Lf)
+            For i As Integer = 0 To results.Count - 1
+                sb.Append(String.Format("_________________________" & ControlChars.Lf))
+                sb.Append(String.Format("Path {0}:" & ControlChars.Lf, i + 1))
+                sb.Append(String.Format("Distance: {0}" & ControlChars.Lf, results(i).Distance))
+                sb.Append(String.Format("Time: {0}" & ControlChars.Lf, results(i).Time))
+                sb.Append(ProcessLegs(results(i).Legs))
+            Next i
+            Return sb.ToString()
+        End Function
+        #End Region ' #ProcessRouteResults
+
+        #Region "#ProcessLegs"
+        Private Function ProcessLegs(ByVal legs As List(Of BingRouteLeg)) As String
+            If legs Is Nothing Then
+                Return ""
+            End If
+
+            Dim sb As New StringBuilder("Legs:" & ControlChars.Lf)
+            For i As Integer = 0 To legs.Count - 1
+                sb.Append(String.Format(ControlChars.Tab & "Leg {0}:" & ControlChars.Lf, i + 1))
+                sb.Append(String.Format(ControlChars.Tab & "Start: {0}" & ControlChars.Lf, legs(i).StartPoint))
+                sb.Append(String.Format(ControlChars.Tab & "End: {0}" & ControlChars.Lf, legs(i).EndPoint))
+                sb.Append(String.Format(ControlChars.Tab & "Distance: {0}" & ControlChars.Lf, legs(i).Distance))
+                sb.Append(String.Format(ControlChars.Tab & "Time: {0}" & ControlChars.Lf, legs(i).Time))
+                sb.Append(ProcessItinerary(legs(i).Itinerary))
+            Next i
+            Return sb.ToString()
+        End Function
+        #End Region ' #ProcessLegs
+
+        #Region "#ProcessItinerary"
+        Private Function ProcessItinerary(ByVal items As List(Of BingItineraryItem)) As String
+            If items Is Nothing Then
+                Return ""
+            End If
+
+            Dim sb As New StringBuilder(ControlChars.Tab & "Internary Items:" & ControlChars.Lf)
+            For i As Integer = 0 To items.Count - 1
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & "Itinerary {0}:" & ControlChars.Lf, i + 1))
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & "Maneuver: {0}" & ControlChars.Lf, items(i).Maneuver))
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & "Location: {0}" & ControlChars.Lf, items(i).Location))
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & "Instructions: {0}" & ControlChars.Lf, items(i).ManeuverInstruction))
+                sb.Append(ProcessWarnings(items(i).Warnings))
+            Next i
+            Return sb.ToString()
+        End Function
+        #End Region ' #ProcessItinerary
+
+        #Region "#ProcessWarnings"
+        Private Function ProcessWarnings(ByVal warnings As List(Of BingItineraryItemWarning)) As String
+            If warnings Is Nothing Then
+                Return ""
+            End If
+
+            Dim sb As New StringBuilder(ControlChars.Tab & ControlChars.Tab & "Warnings:" & ControlChars.Lf)
+            For i As Integer = 0 To warnings.Count - 1
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & "Warning {0}:" & ControlChars.Lf, i + 1))
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & "Type: {0}" & ControlChars.Lf, warnings(i).Type))
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & "Text: {0}" & ControlChars.Lf, warnings(i).Text))
+
+            Next i
+            Return sb.ToString()
+        End Function
+        #End Region ' #ProcessWarnings
+    End Class
+End Namespace
